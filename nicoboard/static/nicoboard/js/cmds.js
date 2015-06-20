229283c6f221c06bc2af21fb23ee1fb91cb00811
Ken Iikura <keniikura@gmail.com>
2015-06-21 02:44:05 +0900

    second commit

---
+++ nicoboard/static/nicoboard/js/cmds.js
@@ -0,0 +1,104 @@
+var cmdsmeta = {
+    "ue": {
+        type: "queue",
+    },
+    "shita": {
+        type: "queue",
+    },
+    "naka": {
+        type: "queue",
+    },
+
+    "small": {
+        type: "size",
+    },
+    "big": {
+        type: "size",
+    },
+
+    "red": {
+        type: "color"
+    },
+    "pink": {
+        type: "color"
+    },
+    "orange": {
+        type: "color"
+    },
+    "yellow": {
+        type: "color"
+    },
+    "green": {
+        type: "color"
+    },
+    "cyan": {
+        type: "color"
+    },
+    "blue": {
+        type: "color"
+    },
+    "purple": {
+        type: "color"
+    },
+    "black": {
+        type: "color"
+    },
+    "white2": {
+        type: "color"
+    },
+    "niconicowhite": {
+        type: "color"
+    },
+    "red2": {
+        type: "color"
+    },
+    "truered": {
+        type: "color"
+    },
+    "pink2": {
+        type: "color"
+    },
+    "orange2": {
+        type: "color"
+    },
+    "passionorange": {
+        type: "color"
+    },
+    "yellow2": {
+        type: "color"
+    },
+    "madyellow": {
+        type: "color"
+    },
+    "green2": {
+        type: "color"
+    },
+    "elementalgreen": {
+        type: "color"
+    },
+    "cyan2": {
+        type: "color"
+    },
+    "blue2": {
+        type: "color"
+    },
+    "marineblue": {
+        type: "color"
+    },
+    "purple2": {
+        type: "color"
+    },
+    "nobleviolet": {
+        type: "color"
+    },
+    "black2": {
+        type: "color"
+    },
+
+}
+
+
+var cmds = [];
+for (cmd in cmdsmeta){
+    cmds.push(cmd);
+}
